70963c8133aab1aef1ebc48333be360c3ef1a03e
notfake777-xp <pereirasalatiel013@gmail.com>
2026-08-23 23:00:16 -0300

    feat: finalizar questão 9

---
+++ Atividade; SALATIEL FILHO - CC/questao09.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, soma = 0;
+
+    printf("N: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        soma += i;
+    }
+
+    printf("Soma: %d\n", soma);
+
+    return 0;
+}
