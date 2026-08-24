7b2f3b6cfe7f7c98f7bbfa7c9f2a476a4c7d2364
notfake777-xp <pereirasalatiel013@gmail.com>
2026-08-23 22:49:20 -0300

    feat: finalizar questão 01

---
+++ Atividade; SALATIEL FILHO - CC/questao01.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main() {
+    int a, b, soma;
+
+    printf("Primeiro valor:  ");
+    scanf("%d", &a);
+
+    print("Segundo valor: ");
+    scanf("%d", &b);
+
+    soma = a + b;
+
+    printf("Soma: %d\n" , soma);
+
+    return 0;
+    
+}
